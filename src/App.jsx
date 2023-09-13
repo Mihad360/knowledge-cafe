1a53826980dc8cde50dab2e81ff5654eb5c5aa48
Mihad360 <ahmedmihad962@gmail.com>
2023-09-14 02:37:22 +0600

    first

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Blogs from './components/Blogs/Blogs'
+import Header from './components/header/header'
+
+function App() {
+
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <Header></Header>
+      <Blogs></Blogs>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,11 @@
+
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className="text-[#111] text-5xl font-bold">Knowledge Cafe</h1>
+        </div>
+    );
+};
+
+export default Header;
